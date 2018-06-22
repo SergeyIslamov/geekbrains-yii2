03f31c7297bf924bb166b883c0f4ce3d31803ae3
Sergey Islamov <sibirium@mail.ru>
2018-06-22 15:20:08 +0000

    Make index.php redirect to the yii2 basic project

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-
-    phpinfo();
-
+header('HTTP/1.1 307 Temporary Redirect');
+header('Location: basic/web/');
 ?>
+<html>
+  <head>
+    <title>Moved</title>
+  </head>
+  <body>
+    <h1>Moved</h1>
+    <p>This page has moved to <a href="basic/web/">Basic</a>.</p>
+  </body>
+</html>
